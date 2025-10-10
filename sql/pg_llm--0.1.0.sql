9ecf90fb2f9c790dd5f1d44a3ed893f8167023ff
Sean Evans <sean.w.evans@gmail.com>
2025-10-10 11:40:58 -0400

    Add dropout support to training forward pass

--- sql/pg_llm--0.1.0.sql
+++ sql/pg_llm--0.1.0.sql
@@ -174,6 +174,222 @@ CREATE UNLOGGED TABLE llm_dataset (
            OR array_length(tokens, 1) = array_length(target, 1))
 );
 
+CREATE OR REPLACE FUNCTION llm_block_forward(
+    input BYTEA,
+    w_qkv BYTEA,
+    b_qkv BYTEA,
+    w_o BYTEA,
+    b_o BYTEA,
+    w_fc BYTEA,
+    b_fc BYTEA,
+    w_proj BYTEA,
+    b_proj BYTEA,
+    ln1_g BYTEA,
+    ln1_b BYTEA,
+    ln2_g BYTEA,
+    ln2_b BYTEA,
+    n_head INT,
+    T INT,
+    D INT,
+    eps FLOAT4 DEFAULT 1e-5,
+    dropout_p FLOAT4 DEFAULT 0.1,
+    training BOOLEAN DEFAULT false)
+RETURNS BYTEA AS $$
+DECLARE
+    x BYTEA := input;
+    attn BYTEA;
+    mlp BYTEA;
+    residual2 BYTEA;
+BEGIN
+    -- 1. LayerNorm
+    x := pg_llm_layernorm(x, ln1_g, ln1_b, eps);
+
+    -- 2. Self-Attention
+    attn := pg_llm_attention(x, w_qkv, b_qkv, w_o, b_o, n_head, T, D);
+    IF training AND dropout_p > 0 THEN
+        attn := pg_llm_dropout(attn, dropout_p, training);
+    END IF;
+    x := pg_llm_add(input, attn);  -- residual 1
+
+    -- 3. LayerNorm
+    residual2 := x;
+    x := pg_llm_layernorm(x, ln2_g, ln2_b, eps);
+
+    -- 4. Feed-Forward MLP
+    mlp := pg_llm_matmul(x, w_fc, T, D, 4*D);
+    mlp := pg_llm_add(mlp, b_fc);
+    mlp := pg_llm_gelu(mlp);
+    mlp := pg_llm_matmul(mlp, w_proj, T, 4*D, D);
+    mlp := pg_llm_add(mlp, b_proj);
+    IF training AND dropout_p > 0 THEN
+        mlp := pg_llm_dropout(mlp, dropout_p, training);
+    END IF;
+
+    x := pg_llm_add(residual2, mlp);       -- residual 2
+    RETURN x;
+END;
+$$ LANGUAGE plpgsql STRICT;
+
+CREATE OR REPLACE FUNCTION llm_forward_gpt2(
+    input BYTEA,
+    model TEXT,
+    n_layer INT,
+    n_head INT,
+    T INT,
+    D INT,
+    ln_f_weight BYTEA DEFAULT NULL,
+    ln_f_bias BYTEA DEFAULT NULL,
+    dropout_p FLOAT4 DEFAULT 0.1,
+    training BOOLEAN DEFAULT false)
+RETURNS BYTEA AS $$
+DECLARE
+    x BYTEA := input;
+    w_qkv BYTEA;
+    b_qkv BYTEA;
+    w_o BYTEA;
+    b_o BYTEA;
+    w_fc BYTEA;
+    b_fc BYTEA;
+    w_proj BYTEA;
+    b_proj BYTEA;
+    ln1_g BYTEA;
+    ln1_b BYTEA;
+    ln2_g BYTEA;
+    ln2_b BYTEA;
+    b_fc_full BYTEA;
+    b_proj_full BYTEA;
+    expected_fc_bytes INT := T * 4 * D * 4;
+    expected_proj_bytes INT := T * D * 4;
+    per_token_fc_bytes INT := 4 * D * 4;
+    per_token_proj_bytes INT := D * 4;
+    final_weight BYTEA := ln_f_weight;
+    final_bias BYTEA := ln_f_bias;
+BEGIN
+    FOR i IN 0..(n_layer-1) LOOP
+        SELECT data INTO w_qkv FROM llm_tensor WHERE name = format('h.%s.attn.c_attn.weight', i);
+        SELECT data INTO b_qkv FROM llm_tensor WHERE name = format('h.%s.attn.c_attn.bias', i);
+        IF b_qkv IS NULL THEN
+            SELECT data INTO b_qkv FROM llm_param WHERE name = format('h.%s.attn.c_attn.bias', i) AND token_id = 0 LIMIT 1;
+        END IF;
+        IF b_qkv IS NULL THEN
+            RAISE EXCEPTION 'Missing attention qkv bias for layer %', i;
+        END IF;
+        SELECT data INTO w_o FROM llm_tensor WHERE name = format('h.%s.attn.c_proj.weight', i);
+        SELECT data INTO b_o FROM llm_tensor WHERE name = format('h.%s.attn.c_proj.bias', i);
+        IF b_o IS NULL THEN
+            SELECT data INTO b_o FROM llm_param WHERE name = format('h.%s.attn.c_proj.bias', i) AND token_id = 0 LIMIT 1;
+        END IF;
+        IF b_o IS NULL THEN
+            RAISE EXCEPTION 'Missing attention proj bias for layer %', i;
+        END IF;
+        SELECT data INTO w_fc FROM llm_tensor WHERE name = format('h.%s.mlp.c_fc.weight', i);
+        SELECT data INTO b_fc FROM llm_tensor WHERE name = format('h.%s.mlp.c_fc.bias', i);
+        SELECT data INTO w_proj FROM llm_tensor WHERE name = format('h.%s.mlp.c_proj.weight', i);
+        SELECT data INTO b_proj FROM llm_tensor WHERE name = format('h.%s.mlp.c_proj.bias', i);
+        SELECT data INTO ln1_g FROM llm_tensor WHERE name = format('h.%s.ln_1.weight', i);
+        SELECT data INTO ln1_b FROM llm_tensor WHERE name = format('h.%s.ln_1.bias', i);
+        SELECT data INTO ln2_g FROM llm_tensor WHERE name = format('h.%s.ln_2.weight', i);
+        SELECT data INTO ln2_b FROM llm_tensor WHERE name = format('h.%s.ln_2.bias', i);
+
+        IF b_fc IS NULL THEN
+            RAISE EXCEPTION 'Missing MLP fc bias for layer %', i;
+        END IF;
+        IF b_proj IS NULL THEN
+            RAISE EXCEPTION 'Missing MLP proj bias for layer %', i;
+        END IF;
+
+        IF octet_length(b_fc) = expected_fc_bytes THEN
+            b_fc_full := b_fc;
+        ELSIF octet_length(b_fc) = per_token_fc_bytes THEN
+            SELECT string_agg(b_fc, ''::bytea) INTO b_fc_full
+            FROM generate_series(1, T);
+        ELSE
+            RAISE EXCEPTION 'MLP fc bias for layer % has % bytes, expected % (broadcasted) or % (per token)',
+                i, octet_length(b_fc), expected_fc_bytes, per_token_fc_bytes;
+        END IF;
+
+        IF octet_length(b_proj) = expected_proj_bytes THEN
+            b_proj_full := b_proj;
+        ELSIF octet_length(b_proj) = per_token_proj_bytes THEN
+            SELECT string_agg(b_proj, ''::bytea) INTO b_proj_full
+            FROM generate_series(1, T);
+        ELSE
+            RAISE EXCEPTION 'MLP proj bias for layer % has % bytes, expected % (broadcasted) or % (per token)',
+                i, octet_length(b_proj), expected_proj_bytes, per_token_proj_bytes;
+        END IF;
+
+        PERFORM pg_llm_autograd_map_param(
+            model,
+            format('h.%s.mlp.c_fc.bias', i),
+            0,
+            b_fc_full,
+            ARRAY[octet_length(b_fc_full) / 4]
+        );
+
+        PERFORM pg_llm_autograd_map_param(
+            model,
+            format('h.%s.mlp.c_proj.bias', i),
+            0,
+            b_proj_full,
+            ARRAY[octet_length(b_proj_full) / 4]
+        );
+
+        x := llm_block_forward(
+            x,
+            w_qkv,
+            b_qkv,
+            w_o,
+            b_o,
+            w_fc,
+            b_fc_full,
+            w_proj,
+            b_proj_full,
+            ln1_g,
+            ln1_b,
+            ln2_g,
+            ln2_b,
+            n_head, T, D,
+            dropout_p => dropout_p,
+            training => training);
+    END LOOP;
+    IF final_weight IS NULL THEN
+        SELECT data INTO final_weight
+        FROM llm_tensor
+        WHERE name = 'ln_f.weight'
+        LIMIT 1;
+    END IF;
+
+    IF final_weight IS NULL THEN
+        SELECT data INTO final_weight
+        FROM llm_param
+        WHERE name = 'ln_f.weight'
+          AND token_id = 0
+        LIMIT 1;
+    END IF;
+
+    IF final_bias IS NULL THEN
+        SELECT data INTO final_bias
+        FROM llm_tensor
+        WHERE name = 'ln_f.bias'
+        LIMIT 1;
+    END IF;
+
+    IF final_bias IS NULL THEN
+        SELECT data INTO final_bias
+        FROM llm_param
+        WHERE name = 'ln_f.bias'
+          AND token_id = 0
+        LIMIT 1;
+    END IF;
+
+    IF final_weight IS NULL OR final_bias IS NULL THEN
+        RAISE EXCEPTION 'Missing ln_f parameters for final layernorm';
+    END IF;
+
+    RETURN pg_llm_layernorm(x, final_weight, final_bias, 1e-5);
+END;
+$$ LANGUAGE plpgsql;
+
 CREATE OR REPLACE FUNCTION llm_loss(
     model TEXT,
     tokens INT[],
